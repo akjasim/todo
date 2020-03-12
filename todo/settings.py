166e2e9055d7590434a9209a5296ef8b8ef08b8f
akjasim <jasimjasim786@gmail.com>
2020-03-12 18:10:34 +0530

    minor refactor

--- todo/settings.py
+++ todo/settings.py
@@ -72,12 +72,6 @@ WSGI_APPLICATION = 'todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
